07644d2bcb3a086c7ede2229df46d8d0f4f7d751
arthurvanstrien <arthurvanstrien@gmail.com>
2014-11-19 16:32:43 +0100

    Worked on Sliders on Artikelen Page

--- html/script/artikelen.js
+++ html/script/artikelen.js
@@ -1,6 +1,20 @@
+$(document).ready(function() 
+{
+	$( ".artikelen p" ).hide();
+});
 
-$("p").hover
-(
-	$("p").css( "color","red" ), 
-	$( this ).css( "color","black" )
-);
+$( "#js-hover1,#js-hover2,#js-hover3,#js-hover4,#js-hover5,#js-hover6,#js-hover7,#js-hover8,#js-hover9" ).hover( hover_in, hover_out);
+
+function hover_in()
+{
+	$( ".artikelen h2" ).css( "top", "0px" );
+	$( ".artikelen p" ).show();
+	//$( ".artikelen p" ).SlideDown();
+}
+
+function hover_out()
+{
+	$( ".artikelen p" ).hide();
+	$( ".artikelen h2" ).css( "top", "210px" );
+	//$( ".artikelen p" ).SlideUp();
+}
