f2e0a4fd2eb3cfc3963724b54299753d44b58f3c
arthurvanstrien <arthurvanstrien@gmail.com>
2014-11-21 15:33:59 +0100

    Finished Artikelen page. Artikelen page is now ready for testing.

--- html/script/artikelen.js
+++ html/script/artikelen.js
@@ -1,21 +1,14 @@
-$(document).ready(function() 
-{
-	$( ".artikelen p" ).hide();
-});
-
 $( "#js-hover1,#js-hover2,#js-hover3,#js-hover4,#js-hover5,#js-hover6,#js-hover7,#js-hover8,#js-hover9" ).hover( hover_in, hover_out);
 
 function hover_in()
 {
-	$(this).find('h2').css( "top", "0px" );
-	$(this).find('p').show();
-	//$( ".artikelen p" ).SlideDown();
+	//$(this).find('h2').css( "top", "0px" );
+	$(this).find('.articleContent').stop().animate({marginTop: "0px"}, 400);
+	$(this).find('p').stop().animate({height: "210px"}, 400);
 }
 
 function hover_out()
 {
-	$(this).find('p').hide();
-	$(this).find('h2').css( "top", "210px" );
-	//$( ".artikelen p" ).SlideUp();
+	$(this).find('.articleContent').stop().animate({marginTop: "210"}, 400);
+	$(this).find('p').stop().animate({height: "0"}, 400);
 }
-
