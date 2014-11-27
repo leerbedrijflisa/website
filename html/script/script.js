7dc28a94c4351dc2fc3b2e0f1258ea7c363cb095
arthurvanstrien <arthurvanstrien@gmail.com>
2014-11-27 10:56:58 +0100

    made the menu for mobile devices

--- html/script/script.js
+++ html/script/script.js
@@ -1,11 +1,12 @@
 $( "#responsive-close a" ).click(hide_menu);
+$( "#responsive-menu" ).click(show_menu);
 
 function show_menu()
 {
-	
+	$(".menu").css( "display", "inline" );
 }
 
 function hide_menu()
 {
-	$(.menu).css( "display", "none" );
+	$(".menu").css( "display", "none" );
 }
