7fb9d497ebb2d5dd3c283d0738ca96a53f8c76f0
arthurvanstrien <arthurvanstrien@gmail.com>
2014-12-11 11:56:32 +0100

    Fixed mobile menu that there is still a usable menu without javascript available or enabled

--- html/script/script.js
+++ html/script/script.js
@@ -1,12 +1,28 @@
-$( "#responsive-close a" ).click(hide_menu);
-$( "#responsive-menu" ).click(show_menu);
+$(document).ready(javascript_enabled);
+$(window).resize(javascript_enabled);
+$( "#responsive-close" ).click(hide_menu);
+$( "#responsive-button" ).click(show_menu);
 
 function show_menu()
 {
-	$(".menu").css( "display", "inline" );
+	$(".responsive-menu").css( "display", "inline" );
 }
 
 function hide_menu()
 {
-	$(".menu").css( "display", "none" );
+	$(".responsive-menu").css( "display", "none" );
+}
+
+function javascript_enabled()
+{
+	if($(document).width() < 801)
+	{
+		$("#responsive-button").css( "display", "inline" );
+		$(".menu").css( "display", "none" );
+	}
+	else
+	{
+		$("#responsive-button").css( "display", "none" );
+		$(".menu").css( "display", "inline" );
+	}
 }
