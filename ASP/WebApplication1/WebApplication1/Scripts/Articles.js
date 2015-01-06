7948b7ac184b465c1bcc18cb3697d303595f716b
arthurvanstrien <arthurvanstrien@gmail.com>
2015-01-06 16:34:11 +0100

    Worked on fixing javascript / jquery in articles page

---
+++ ASP/WebApplication1/WebApplication1/Scripts/Articles.js
@@ -0,0 +1,16 @@
+﻿if (screen.width > 650)
+{
+    $("#articleButton").hover(hover_in, hover_out);
+
+    function hover_in()
+    {
+        $(this).find('.articleInside').stop().animate({ marginTop: "0px" }, 400);
+        $(this).find('p').stop().animate({ height: "210px" }, 400);
+    }
+
+    function hover_out()
+    {
+        $(this).find('.articleInside').stop().animate({ marginTop: "210" }, 400);
+        $(this).find('p').stop().animate({ height: "0" }, 400);
+    }
+}
