05cb8a3562d8d059b4fc246b918e823ca5e116ec
arthurvanstrien <arthurvanstrien@gmail.com>
2015-01-08 10:58:46 +0100

    Fixed Javascript/Jquery bugs in articles page. Explanation: I confused the dot for classes with the hashtag for ID while trying to move an element with an ID and did not see my own fault.

--- ASP/WebApplication1/WebApplication1/Scripts/Articles.js
+++ ASP/WebApplication1/WebApplication1/Scripts/Articles.js
@@ -2,21 +2,21 @@
 {
     var numberOfString = $('.articlesContent').find('li').last().attr('id');
     var numberOf = numberOfString.replace("articleButton", "");
-
+    
     for (i = 1; i <= numberOf; i++)
     {
-        $('articleButton'+i).hover(hover_in, hover_out);
+        $('#articleButton' + i).hover(hover_in, hover_out);
     }
 
     function hover_in()
     {
-        $(this).find('div').stop().animate({ marginTop: "0px" }, 400);
+        $(this).find('h2').stop().animate({ marginTop: "0px" }, 400);
         $(this).find('p').stop().animate({ height: "210px" }, 400);
     }
 
     function hover_out()
     {
-        $(this).find('div').stop().animate({ marginTop: "210" }, 400);
+        $(this).find('h2').stop().animate({ marginTop: "210" }, 400);
         $(this).find('p').stop().animate({ height: "0" }, 400);
     }
 }
