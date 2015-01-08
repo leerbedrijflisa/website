b81b4263e8207e0e26285386a48656fe39381da6
arthurvanstrien <arthurvanstrien@gmail.com>
2015-01-08 09:24:54 +0100

    Working on a bug in the code around the hovering tiles on articles page

--- ASP/WebApplication1/WebApplication1/Scripts/Articles.js
+++ ASP/WebApplication1/WebApplication1/Scripts/Articles.js
@@ -1,16 +1,22 @@
 ﻿if (screen.width > 650)
 {
-    $("#articleButton").hover(hover_in, hover_out);
+    var numberOfString = $('.articlesContent').find('li').last().attr('id');
+    var numberOf = numberOfString.replace("articleButton", "");
+
+    for (i = 1; i <= numberOf; i++)
+    {
+        $('articleButton'+i).hover(hover_in, hover_out);
+    }
 
     function hover_in()
     {
-        $(this).find('.articleInside').stop().animate({ marginTop: "0px" }, 400);
+        $(this).find('div').stop().animate({ marginTop: "0px" }, 400);
         $(this).find('p').stop().animate({ height: "210px" }, 400);
     }
 
     function hover_out()
     {
-        $(this).find('.articleInside').stop().animate({ marginTop: "210" }, 400);
+        $(this).find('div').stop().animate({ marginTop: "210" }, 400);
         $(this).find('p').stop().animate({ height: "0" }, 400);
     }
 }
