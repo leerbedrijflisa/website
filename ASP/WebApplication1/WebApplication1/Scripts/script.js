1be1b49352cc25766ee58a6c62b90793a77b4777
arthurvanstrien <arthurvanstrien@gmail.com>
2015-01-09 21:35:46 +0100

    Fixed menu jump buttons and  fixed href in responsive menu

--- ASP/WebApplication1/WebApplication1/Scripts/script.js
+++ ASP/WebApplication1/WebApplication1/Scripts/script.js
@@ -3,23 +3,28 @@
 $(document).ready(javascript_enabled);
 $(window).resize(javascript_enabled);
 $("#menubutton a").click(hide_menu);
-$("#responsive-button").click(show_menu);
+$("#responsiveButton").click(show_menu);
 
-function show_menu() {
+function show_menu()
+{
     $(".responsive-menu").css("display", "inline");
 }
 
-function hide_menu() {
+function hide_menu()
+{
     $(".responsive-menu").css("display", "none");
 }
 
-function javascript_enabled() {
-    if ($(document).width() < 801) {
-        $("#responsive-button").css("display", "inline");
+function javascript_enabled()
+{
+    if ($(document).width() < 801)
+    {
+        $("#responsiveButton").css("display", "inline");
         $(".menu").css("display", "none");
     }
-    else {
-        $("#responsive-button").css("display", "none");
+    else
+    {
+        $("#responsiveButton").css("display", "none");
         $(".menu").css("display", "inline");
     }
 }
