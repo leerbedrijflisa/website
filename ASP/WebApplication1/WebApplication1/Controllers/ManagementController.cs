dba77d8bf99887780c6e93ba943667c54e6c9744
arthurvanstrien <arthurvanstrien@gmail.com>
2015-01-22 16:40:55 +0100

    Worked on management

---
+++ ASP/WebApplication1/WebApplication1/Controllers/ManagementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Lisa.Website.Controllers
+{
+    public class ManagementController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.css = "";
+            ViewBag.cssResponsive = "";
+            return View();
+        }
+
+        public ActionResult AddArticle()
+        {
+            ViewBag.css = "";
+            ViewBag.cssResponsive = "";
+            return View();
+        }
+
+        [HttpPost]
+        public FileStreamResult AddArticle(Article)
+        {
+            //NOG AFMAKEN!
+        }
+    }
+}
