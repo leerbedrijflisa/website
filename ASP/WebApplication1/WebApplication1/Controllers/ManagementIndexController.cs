57bea4d52b82646814b33a91e6c5909415292328
arthurvanstrien <arthurvanstrien@gmail.com>
2015-02-03 09:09:27 +0100

    Added Main CMS page and connected other pages to that page.

---
+++ ASP/WebApplication1/WebApplication1/Controllers/ManagementIndexController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Lisa.Website.Controllers
+{
+    public class ManagementIndexController : Controller
+    {
+        public ActionResult Index()
+        {
+            SetCSS("Index");
+            return View();
+        }
+
+        private void SetCSS(string pageName)
+        {
+            ViewBag.css = "Index/" + pageName + ".css";
+            ViewBag.cssResponsive = "Index/" + pageName + "Responsive.css";
+        }
+    }
+}
