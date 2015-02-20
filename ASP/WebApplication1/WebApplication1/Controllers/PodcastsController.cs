7c2f960973d218cd78b492f2bccdfca7c7f04a4d
arthurvanstrien <arthurvanstrien@gmail.com>
2015-02-20 15:55:25 +0100

    Refactored Podcasts. Merged podcastcontroller and podcastsmanagementcontroller, renamed the index page of podcastsmanagement to admin, moved all the views in the view folder of the podcastcontroller. Style of podcast still needs to be refactored.

--- ASP/WebApplication1/WebApplication1/Controllers/PodcastsController.cs
+++ ASP/WebApplication1/WebApplication1/Controllers/PodcastsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 
 namespace Lisa.Website.Controllers
@@ -16,10 +17,7 @@ namespace Lisa.Website.Controllers
 
         public ActionResult Details(int id)
         {
-            var podcast = (from a in GetPodcast()
-                           where a.Id == id
-                           select a).SingleOrDefault();
-
+            var podcast = _db.Podcasts.Find(id);
             return View(podcast);
         }
 
@@ -28,10 +26,79 @@ namespace Lisa.Website.Controllers
             return View(GetPodcast());
         }
 
+        public ActionResult Admin()
+        {
+            return View(GetPodcast());
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var podcasts = _db.Podcasts.Find(id);
+
+            if (podcasts == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(podcasts);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, string confirmed)
+        {
+            var podcast = _db.Podcasts.Find(id);
+
+            if (confirmed == "Verwijderen")
+            {
+                _db.Podcasts.Remove(podcast);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Admin");
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Podcast podcast)
+        {
+            _db.Podcasts.Add(new Podcast
+            {
+                Title = podcast.Title,
+                Image = podcast.Image,
+                Content = podcast.Content,
+                Author = podcast.Author,
+                References = podcast.References,
+                File = podcast.File,
+                Date = DateTime.Now
+            });
+            _db.SaveChanges();
+            return RedirectToAction("Admin");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var podcast = _db.Podcasts.Find(id);
+            return View(podcast);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Podcast NewPodcast)
+        {
+            NewPodcast.Id = id;
+            _db.Entry(NewPodcast).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Admin");
+        }
+
         private IEnumerable<Podcast> GetPodcast()
         {
-            var db = new WebsiteContext();
-            return db.Podcasts.AsEnumerable();
+            return _db.Podcasts.AsEnumerable();
         }
+
+        private WebsiteContext _db = new WebsiteContext();
     }
 }
