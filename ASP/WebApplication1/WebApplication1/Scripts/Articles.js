40a1ec8b5e06e34f8f2333da20e1ecea50578b96
arthurvanstrien <arthurvanstrien@gmail.com>
2015-03-25 09:23:14 +0100

    Fixed a javascript bug on article page.

--- ASP/WebApplication1/WebApplication1/Scripts/Articles.js
+++ ASP/WebApplication1/WebApplication1/Scripts/Articles.js
@@ -1,28 +1,38 @@
 ﻿//If checks also if value is a Number to prevent problems.
 
-if ($(window).width() > 800 && $.isNumeric($(window).width()))
+function hover_in()
 {
-    function hover_in()
+    if ($('body').width() > 800 && $.isNumeric($('body').width()))
     {
         $(this).find('h2').stop().animate({ marginTop: "0px" }, 400);
         $(this).find('p').stop().animate({ height: "210px" }, 400);
     }
+}
 
-    function hover_out()
+function hover_out()
+{
+    if ($('body').width() > 800 && $.isNumeric($('body').width()))
     {
         $(this).find('h2').stop().animate({ marginTop: "210px" }, 400);
         $(this).find('p').stop().animate({ height: "0px" }, 400);
     }
-
-    var numberOf = $('.articlesContent').find('a').last().attr('id');
-
-    $(".articlesContent a").hover(hover_in, hover_out);
-
-    /*
-    for (i = 1; i <= numberOf; i++)
-    {
-        $(i).mouseenter(hover_in());
-        $(i).mouseleave(hover_out());
-    }
-    */
 }
+
+function article_hover()
+{
+    if ($('body').width() > 800 && $.isNumeric($('body').width()))
+    {
+        $(".articlesContent h2").css({ marginTop: "210px" });
+        $(".articlesContent p").css({ height: "0px" });
+        var numberOf = $('.articlesContent').find('a').last().attr('id');
+        $(".articlesContent a").hover(hover_in, hover_out).on;
+    }
+    else
+    {
+        $(".articlesContent h2").css({ marginTop: "0px"  }).stop();
+        $(".articlesContent p").css({ height: "auto" });
+    }
+}
+
+$(document).ready(article_hover);
+$( window ).resize(article_hover);
