6e1e80785e74ec54ac7f8726598d05e94250287b
William Willing <william@williamwilling.com>
2015-03-27 11:33:26 +0100

    Database migrations are now always executed when the website starts.

--- ASP/WebApplication1/WebApplication1/Global.asax.cs
+++ ASP/WebApplication1/WebApplication1/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,9 @@ namespace Lisa.Website
     {
         protected void Application_Start()
         {
+            // Make sure the database is updated to the latest version. This effectively runs Update-Database, even when running in Azure.
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<WebsiteContext, Migrations.Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
